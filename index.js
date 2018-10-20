08292c5cb4d81440228c29fea968e0969da048f4
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-10-20 21:46:43 +0200

    feat: add npm packaged & linting

--- index.js
+++ index.js
@@ -1,264 +1,296 @@
 const GrowingFormLayoutDimension = {
-    LEFT_MARGIN: 0,
-    RIGHT_MARGIN: 0,
-    CONTENT_LEFT_MARGIN: 80,
-    CONTENT_RIGHT_MARGIN: 8,
-    CONTENT_TOP_MARGIN: 0,
-    CONTENT_BOTTOM_MARGIN: 16,
-}
+	LEFT_MARGIN: 0,
+	RIGHT_MARGIN: 0,
+	CONTENT_LEFT_MARGIN: 80,
+	CONTENT_RIGHT_MARGIN: 8,
+	CONTENT_TOP_MARGIN: 0,
+	CONTENT_BOTTOM_MARGIN: 16,
+};
 
 const GrowingFormEvent = {
-    STEP: 'step',
-    SUBMIT: 'submit'
+	STEP: 'step',
+	SUBMIT: 'submit'
 };
+
+class FormError extends Error {}
 
 class GrowingForm {
 
-    constructor(options = {}) {
-        // Access configuration parameters, perform some basic validation
-        const configuration = options.configuration
-        const cells = configuration.cells;
+	constructor(options = {}) {
+		// Access configuration parameters, perform some basic validation
+		const configuration = options.configuration;
+		const cells = configuration.cells;
 
-        if (!configuration) throw 'Missing \'configuration\' parameter'
-        if (!cells) throw 'Missing \'cells\' parameter in \'configuration\''
+		if (!configuration) {
+			throw new FormError('Missing \'configuration\' parameter');
+		}
+		if (!cells) {
+			throw new FormError('Missing \'cells\' parameter in \'configuration\'');
+		}
 
-        // Create root view
-        this.formView = Ti.UI.createView();
+		// Link some additional options like bullet styling
+		this.options = configuration.options || {};
 
-        // Style the root view with optional overrides
-        const overrides = configuration.overrides || {};
-        Object.assign(this.formView, overrides, this.formView);
+		// Create root view
+		this.formView = Ti.UI.createView();
 
-        // Set the initial expanded data
-        this.expandedIndex = 0;
-        this.cells = cells;
-        
-        // Configure the callbacks object to be used by "on" and "off" methods
-        this.callbacks = {};
+		// Initialize the form-data
+		this.formData = {};
 
-        // Create the container table-view with our data source
-        this.tableView = Ti.UI.createTableView({ 
-            separatorStyle: Ti.UI.TABLE_VIEW_SEPARATOR_STYLE_NONE,
-            selectionStyle: 0
-        });
-        this.formView.add(this.tableView);
+		// Style the root view with optional overrides
+		const overrides = configuration.overrides || {};
+		Object.assign(this.formView, overrides, this.formView);
 
-        // Configure the initial data set
-        this._configureData();
-    }
+		// Set the initial expanded data
+		this.expandedIndex = 0;
+		this.cells = cells;
 
-    on(eventName, cb) {
-        if (this.callbacks[eventName] !== undefined) {
-            throw 'Duplicate event listener found. Please use \'off\' to clean up your listener'
-        }
-        this.callbacks[eventName] = cb;
-    }
+		// Configure the callbacks object to be used by "on" and "off" methods
+		this.callbacks = {};
 
-    off(eventName, cb) {
-        if (this.callbacks[eventName] === undefined) {
-            throw 'No event listener found. Please use \'on\' to configure up your listener'
-        }
-        delete this.callbacks[eventName];
-    }
+		// Create the container table-view with our data source
+		this.tableView = Ti.UI.createTableView({
+			backgroundColor: 'transparent',
+			separatorStyle: Ti.UI.TABLE_VIEW_SEPARATOR_STYLE_NONE,
+			selectionStyle: 0
+		});
+		this.formView.add(this.tableView);
 
-    _configureData() {
-        let data = [];
-        let itemIndex = 0;
+		// Configure the initial data set
+		this._configureData();
+	}
 
-        this.cells.forEach(cell => {
-            // Perform cell validation
-            if (!cell.title) throw 'Missing \'title\' parameter in cell configuration';
-            if (!cell.type) throw 'Missing \'type\' parameter in cell configuration';
+	on(eventName, cb) {
+		if (this.callbacks[eventName] !== undefined) {
+			throw new FormError('Duplicate event listener found. Please use \'off\' to clean up your listener');
+		}
+		this.callbacks[eventName] = cb;
+	}
 
-            // Create a basic row
-            const row = Ti.UI.createTableViewRow({ height: Ti.UI.SIZE });
+	off(eventName, cb) {
+		if (this.callbacks[eventName] === undefined || this.callbacks[eventName] !== cb) {
+			throw new FormError('No event listener found. Please use \'on\' to configure up your listener');
+		}
+		delete this.callbacks[eventName];
+	}
 
-            // Create and assign the content view
-            const contentView = this._createContentView(cell, itemIndex, itemIndex === this.expandedIndex);
-            row.add(contentView);
+	_configureData() {
+		let data = [];
+		let itemIndex = 0;
 
-            // Push the row to our data source
-            data.push(row);
-            itemIndex++;
-        });
+		this.cells.forEach(cell => {
+			// Perform cell validation
+			if (!cell.title) {
+				throw new FormError('Missing \'title\' parameter in cell configuration');
+			}
+			if (!cell.type) {
+				throw new FormError('Missing \'type\' parameter in cell configuration');
+			}
+			if (!cell.identifier) {
+				throw new FormError('Missing \'identifier\' parameter in cell configuration');
+			}
 
-        this.tableView.data = data;
-    }
+			// Create a basic row
+			const row = Ti.UI.createTableViewRow({ height: Ti.UI.SIZE });
 
-    render(options = {}) {
-        const view = options.view;
+			// Create and assign the content view
+			const contentView = this._createContentView(cell, itemIndex, itemIndex === this.expandedIndex);
+			row.add(contentView);
 
-        // Validate container view
-        if (!view) throw 'Missing required \'view\' parameter'
+			// Push the row to our data source
+			data.push(row);
+			itemIndex++;
+		});
 
-        // Validate of the instance of this form has already been rendered in the container
-        view.children.forEach(child => {
-            if (child === this.formView) throw 'Cannot render form in container view twice'; 
-        });
+		this.tableView.data = data;
+	}
 
-        // Add the instance of this form to the container
-        view.add(this.formView);
-    }
+	render(options = {}) {
+		const view = options.view;
 
-    _createContentView(cell, itemIndex = -1, isExpanded) {
-        const title = cell.title;
-        const type = cell.type;
-        const options = cell.options;
+		// Validate container view
+		if (!view) {
+			throw new FormError('Missing required \'view\' parameter');
+		}
 
-        // The container view holds both the content-view and bullets
-        const containerView = Ti.UI.createView({ 
-            height: Ti.UI.SIZE, 
-            left: GrowingFormLayoutDimension.LEFT_MARGIN, 
-            right: GrowingFormLayoutDimension.RIGHT_MARGIN
-        });
+		// Validate of the instance of this form has already been rendered in the container
+		view.children.forEach(child => {
+			if (child === this.formView) {
+				throw new FormError('Cannot render form in container view twice');
+			}
+		});
 
-        const contentView = Ti.UI.createView({
-            top: GrowingFormLayoutDimension.CONTENT_TOP_MARGIN,
-            right: GrowingFormLayoutDimension.CONTENT_RIGHT_MARGIN,
-            bottom: GrowingFormLayoutDimension.CONTENT_BOTTOM_MARGIN,
-            left: GrowingFormLayoutDimension.CONTENT_LEFT_MARGIN,
-            height: Ti.UI.SIZE,
-            layout: 'vertical',
-        })
+		// Add the instance of this form to the container
+		view.add(this.formView);
+	}
 
-        contentView.add(this._createTitleLabel(title));
+	_createContentView(cell, itemIndex = -1, isExpanded) {
+		const title = cell.title;
+		const type = cell.type;
+		const identifier = cell.identifier;
+		const options = cell.options;
 
-        if (isExpanded) {
-            switch (type) {
-                case GrowingFormFieldType.TEXT: {
-                    contentView.add(this._createTextField(options));
-                    break;
-                }
-                default: {
-                    throw `Unhandled cell type = ${type.toUpperCase()}`;
-                } 
-            }
+		// The container view holds both the content-view and bullets
+		const containerView = Ti.UI.createView({
+			height: Ti.UI.SIZE,
+			left: GrowingFormLayoutDimension.LEFT_MARGIN,
+			right: GrowingFormLayoutDimension.RIGHT_MARGIN
+		});
 
-            contentView.add(this._createActionButton(itemIndex, {
-                onClick: event => {
-                    this._selectNextCell(event);
-                }
-            }));
-        }
+		const contentView = Ti.UI.createView({
+			top: GrowingFormLayoutDimension.CONTENT_TOP_MARGIN,
+			right: GrowingFormLayoutDimension.CONTENT_RIGHT_MARGIN,
+			bottom: GrowingFormLayoutDimension.CONTENT_BOTTOM_MARGIN,
+			left: GrowingFormLayoutDimension.CONTENT_LEFT_MARGIN,
+			height: Ti.UI.SIZE,
+			layout: 'vertical',
+		});
 
-        // Change the separator height based opn the content height, shesshh!
-        containerView.addEventListener('postlayout', event => {
-            separatorLine.height = event.source.rect.height.toFixed(0);
-        });
+		contentView.add(this._createTitleLabel(title));
 
-        const bulletView = Ti.UI.createView({
-            width: 36,
-            height: 36,
-            backgroundColor: '#000',
-            borderRadius: 18,
-            top: 0,
-            center: {
-                x: GrowingFormLayoutDimension.CONTENT_LEFT_MARGIN / 2.0
-            }
-        });
+		if (isExpanded) {
+			switch (type) {
+				case GrowingFormFieldType.TEXT: {
+					contentView.add(this._createTextField(identifier, options));
+					break;
+				}
+				default: {
+					throw new FormError(`Unhandled cell type = ${type.toUpperCase()}`);
+				}
+			}
 
-        const separatorLine = Ti.UI.createView({
-            top: 0,
-            left: GrowingFormLayoutDimension.CONTENT_LEFT_MARGIN / 2.0,
-            width: 1,
-            height: Ti.UI.SIZE,
-            backgroundColor: '#ccc',
-            bottom: 0
-        });
+			contentView.add(this._createActionButton(itemIndex, {
+				onClick: () => {
+					this._selectNextCell();
+				}
+			}));
+		}
 
-        bulletView.add(Ti.UI.createLabel({
-            text: `${itemIndex + 1}`,
-            color: '#fff',
-            font: {
-                fontSize: 12
-            }
-        }));
+		// Change the separator height based opn the content height, shesshh!
+		containerView.addEventListener('postlayout', event => {
+			separatorLine.height = event.source.rect.height.toFixed(0);
+		});
 
-        containerView.add(contentView);
+		const bulletView = Ti.UI.createView({
+			width: 36,
+			height: 36,
+			backgroundColor: isExpanded ? this.options.bulletActiveBackgroundColor : this.options.bulletInactiveBackgroundColor,
+			borderRadius: 18,
+			top: 0,
+			center: {
+				x: GrowingFormLayoutDimension.CONTENT_LEFT_MARGIN / 2.0
+			}
+		});
 
-        // Do not add the separator line for the last item
-        if (itemIndex !== this.cells.length -1) {
-            containerView.add(separatorLine);
-        }
+		const separatorLine = Ti.UI.createView({
+			top: 0,
+			left: GrowingFormLayoutDimension.CONTENT_LEFT_MARGIN / 2.0,
+			width: 1,
+			height: Ti.UI.SIZE,
+			backgroundColor: '#ccc',
+			bottom: 0
+		});
 
-        containerView.add(bulletView);
+		bulletView.add(Ti.UI.createLabel({
+			text: `${itemIndex + 1}`,
+			color: '#fff',
+			font: {
+				fontSize: 12
+			}
+		}));
 
-        return containerView;
-    }
+		containerView.add(contentView);
 
-    _selectNextCell(event = {}) {
-        // If we are done, trigger the "submit" callback
-        if (this.expandedIndex === this.cells.length - 1) {
-            if (this.callbacks[GrowingFormEvent.SUBMIT]) {
-                // TODO: Return form data
-                const submitData = { 'TODO': 'RETURN_FORM_DATA' };
-                this.callbacks[GrowingFormEvent.SUBMIT](submitData);
-            }
-            alert('DONE!');
-            return;
-        // If not, yet, trigger the "step" callback
-        } else {
-            if (this.callbacks[GrowingFormEvent.STEP]) {
-                this.callbacks[GrowingFormEvent.STEP](this.expandedIndex + 1);
-            }
-        }
+		// Do not add the separator line for the last item
+		if (itemIndex !== this.cells.length - 1) {
+			containerView.add(separatorLine);
+		}
 
-        this.expandedIndex++;
-        this._configureData();
-    }
+		containerView.add(bulletView);
 
-    _createTextField(options = {}) {
-        const textField = Ti.UI.createTextField({
-            top: 8,
-            left: 0,
-            width: 280,
-            height: 40,
-            padding: { left: 16 },
-            borderRadius: 4,
-            backgroundColor: '#eee'
-        });
+		return containerView;
+	}
 
-        Object.assign(textField, options, textField);
+	_selectNextCell() {
+		// If we are done, trigger the "submit" callback
+		if (this.expandedIndex === this.cells.length - 1) {
+			// If the last element was a text-field, blur it!
+			if (this.cells[this.expandedIndex].type === GrowingFormFieldType.TEXT && this.currentTextField) {
+				this.currentTextField.blur();
+			}
+			if (this.callbacks[GrowingFormEvent.SUBMIT]) {
+				this.callbacks[GrowingFormEvent.SUBMIT](this.formData);
+			}
+			return;
+			// If not, yet, trigger the "step" callback
+		} else if (this.callbacks[GrowingFormEvent.STEP]) {
+			this.callbacks[GrowingFormEvent.STEP](this.expandedIndex + 1);
+		}
 
-        return textField;
-    } 
+		this.expandedIndex++;
+		this._configureData();
+	}
 
-    _createTitleLabel(title = L('Select')) {
-       const titleLabel = Ti.UI.createLabel({
-           top: 6, 
-           left: 0, 
-           text: title
-        });
+	_createTextField(identifier, options = {}) {
+		const textField = Ti.UI.createTextField({
+			top: 8,
+			left: 0,
+			width: 280,
+			height: 40,
+			padding: { left: 16 },
+			borderRadius: 4,
+			backgroundColor: '#eee'
+		});
 
-        return titleLabel;
-    }
+		// Reference a reference in our scope to blur it, if it is the last form input
+		this.currentTextField = textField;
 
-    _createActionButton(itemIndex, options = {}) {
-        const actionButton = Ti.UI.createButton({
-            title: L('Continue'),
-            width: 120,
-            height: 40,
-            borderRadius: 20,
-            backgroundColor: '#555',
-            color: '#fff',
-            top: 10,
-            left: 0
-        });
+		textField.addEventListener('change', event => {
+			this.formData[identifier] = event.value;
+		});
 
-        if (itemIndex === this.cells.length - 1) {
-            actionButton.title = L('Done');
-        }
+		Object.assign(textField, options, textField);
 
-        actionButton.addEventListener('click', options.onClick);
+		return textField;
+	}
 
-        return actionButton;
-    }
+	_createTitleLabel(title = L('Select')) {
+		const titleLabel = Ti.UI.createLabel({
+			top: 6,
+			left: 0,
+			text: title
+		});
+
+		return titleLabel;
+	}
+
+	_createActionButton(itemIndex, options = {}) {
+		const actionButton = Ti.UI.createButton({
+			title: L('Continue'),
+			width: 120,
+			height: 40,
+			borderRadius: 20,
+			backgroundColor: this.options.stepButtonBackgroundColor,
+			color: '#fff',
+			top: 10,
+			left: 0
+		});
+
+		if (itemIndex === this.cells.length - 1) {
+			actionButton.title = L('Submit');
+			actionButton.backgroundColor = this.options.submitButtonBackgroundColor;
+		}
+
+		actionButton.addEventListener('click', options.onClick);
+
+		return actionButton;
+	}
 }
 
 const GrowingFormFieldType = {
-    TEXT: 'text',
-    CHECKBOX: 'checkbox',
-    DROPDOWN: 'dropdown'
+	TEXT: 'text',
+	CHECKBOX: 'checkbox',
+	DROPDOWN: 'dropdown'
 };
 
-export { GrowingForm, GrowingFormFieldType } 
+export { GrowingForm, GrowingFormFieldType };
